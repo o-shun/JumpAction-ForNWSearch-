1cb880455fe6c8573e0418339cb641ea90225cde
COLOR <zyaooo99@yahoo.co.jp>
2020-02-26 13:35:13 +0900

    更新版 2/26

---
+++ Assets/Script/TsetScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TsetScript : MonoBehaviour {
+
+	[SerializeField]
+	private int akahira = 0;
+	[SerializeField]
+	private int otsuka = 0;
+
+	void Start ()
+    {
+
+	}
+
+	void Update ()
+    {
+		
+	}
+}
